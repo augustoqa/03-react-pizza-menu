4e8167d0bcd7b5f879d5611843a89ee0d5746710
Augusto Quisquinay <augustoacual@gmail.com>
2024-05-19 10:32:28 -0600

    Create React Fragment

--- src/index.js
+++ src/index.js
@@ -78,11 +78,17 @@ function Menu() {
       <h2>Our menu</h2>
 
       {numPizzas > 0 ? (
-        <ul className='pizzas'>
-          {pizzas.map((pizza) => (
-            <Pizza key={pizza.name} pizzaObj={pizza} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className='pizzas'>
+            {pizzas.map((pizza) => (
+              <Pizza key={pizza.name} pizzaObj={pizza} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later :)</p>
       )}
