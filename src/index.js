d4df33d94e15bfc9b7d7d59dd7d622b5e1c6b7d8
Augusto Quisquinay <augustoacual@gmail.com>
2024-05-19 09:21:12 -0600

    Conditional Rendering with &&

--- src/index.js
+++ src/index.js
@@ -70,23 +70,39 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData
+  const numPizzas = pizzas.length
+
   return (
     <main className='menu'>
       <h2>Our menu</h2>
 
-      <ul className='pizzas'>
-        {pizzaData.map((pizza) => (
-          <Pizza key={pizza.name} pizzaObj={pizza} />
-        ))}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className='pizzas'>
+          {pizzas.map((pizza) => (
+            <Pizza key={pizza.name} pizzaObj={pizza} />
+          ))}
+        </ul>
+      )}
     </main>
   )
 }
 
 function Footer() {
+  const hour = new Date().getHours()
+  const openHour = 9
+  const closeHour = 22
+  const isOpen = hour >= openHour && hour <= closeHour
+  console.log(isOpen)
+
   return (
     <footer className='footer'>
-      {new Date().toLocaleTimeString()} We're currently open
+      {isOpen && (
+        <div className='order'>
+          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+          <button className='btn'>Order</button>
+        </div>
+      )}
     </footer>
   )
 }
